26dbc00c623933ed4cd1931a2bf2ec351d5373be
DiegoLonguechaux <diego.longuechaux@gmail.com>
2025-02-06 22:53:42 +0100

    up

--- bandnest-app/app/Providers/AppServiceProvider.php
+++ bandnest-app/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Gate::define('viewApiDocs', function (User $user) {
+            return $user->hasRole('super_admin');
+        });
         Vite::prefetch(concurrency: 3);
     }
 }
